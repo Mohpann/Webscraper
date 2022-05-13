20d9ea33546212c58c4c4e0c42267172f2289421
Mohpann <mohpann@gmail.com>
2022-05-13 01:34:07 +0000

    Refactored my GoogleFinance scraper into an object.

--- GOOGscraper.py
+++ GOOGscraper.py
@@ -2,83 +2,35 @@ import requests
 from bs4 import BeautifulSoup 
 from urllib.request import Request, urlopen
 import re
+import GoogleScraper #OR from MODULE GoogleScraper import CLASS GoogleScraper
+
+#import GoogleScraper imports the WHOLE file, you want to import the class WITHIN the file
+
 #https://investoracademy.org/top-10-fundamental-analysis-indicators-for-all-investors/ -> good fundamental analysis indicators
 #https://www.getsmarteraboutmoney.ca/invest/investment-products/stocks/6-indicators-used-to-assess-stocks/ -> same thing but with formulas
 
-ticker = input("Enter the Stock Ticker you'd like financial information on: ")
-url = f"https://www.google.com/finance/quote/{ticker}:NYSE?hl=en"
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-webpage = urlopen(req).read()
-soup = BeautifulSoup(webpage,'html.parser')
-
-pdiv = soup.find_all('div', {'jsaction': 'JIbuQc:YxFE3d(IAcfIc),crCqv(kl0Ckd),sko5qc(vsmyDc);rcuQ6b:npT2md;GvneHb:GvneHb', 'jscontroller': 'glDwzc', 'class': 'GRaO7d'})
-table = pdiv[0].contents[3]
-
 #tContents = table.contents[3]
 print() #for separation of outputs in terminal 
-def getEPS():
-    for tr in table:
-        for td in tr:
-            if (td.find(text=re.compile("Diluted EPS"))):
-                output = td.text
-                ratio = td.parent.find_all('td')[1].text
-    return(output + ": " + ratio)
-print(getEPS())
-print()
-def getNetIncome():
-    NIrow = table.contents[2]
-    NITitle = NIrow.find('div').text
-    NetIncome = NIrow.contents[1].text
-    NetGrowth = NIrow.contents[2].find('span').text
-    print(NITitle + ": " + NetIncome)
-    print(NetGrowth)
-    NetGrowth = NetGrowth.replace('%', '')
-    NetGrowth = float(NetGrowth)
-    if (NetGrowth >= 20):
-        return('Net Income Growth is good!')
-    elif(NetGrowth >= 10):
-        return('Net Income Growth is healthy, but could be better.')
-    elif(NetGrowth >= 5):
-        return('Net Income Growth is low.')
-    elif(NetGrowth >= 0):
-        return('Net Income Growth is not good.')
-    else:
-        return('Net Income Growth is negative. Not good at all!')
-print(getNetIncome())
-print()
-def getPERatio():
-    stockInfo = soup.find('div', {'class': "eYanAe", 'role': 'region', 'aria-labelledby': 'key-stats-heading'})
-    stockInfo.find_all('div')
-    infoRows = stockInfo.find_all('div', class_='gyFHrc')
-    PErow = infoRows[5]
-    PETitle = PErow.contents[0].contents[0].text
-    PERatio = PErow.contents[1].text
-    print(PETitle + ": " + PERatio)
-    if (int(float(PERatio)) >= 20):
-        return('P/E ratio is good!')
-    else:
-        return('P/E ratio is not good!')
-print(getPERatio())
+ticker = "ORCL"
+scraper = GoogleScraper.GoogleScraper(ticker)
+print(scraper.getEPS())
 print()
 
-def getDivYield():
-    stockInfo = soup.find('div', {'class': "eYanAe", 'role': 'region', 'aria-labelledby': 'key-stats-heading'})
-    stockInfo.find_all('div')
-    infoRows = stockInfo.find_all('div', class_='gyFHrc')
-    DivRow = infoRows[6]
-    DivTitle = DivRow.contents[0].contents[0].text
-    DivYield = DivRow.contents[1].text
-    return(DivTitle + ": " + DivYield)
-print(getDivYield())
+print(scraper.getNetIncome())
+print()
+
+print(scraper.getPERatio())
+print()
+
+print(scraper.getDivYield())
 print()
 
 f = open("demofile.txt", "x")
 f = open("demofile.txt", "a") # append
-f.write(getEPS())
-f.write(getNetIncome())
-f.write(getPERatio())
-f.write(getDivYield())
+f.write(scraper.getEPS())
+f.write(scraper.getNetIncome())
+f.write(scraper.getPERatio())
+f.write(scraper.getDivYield())
 f.close()
 #isPEGood
 #20-25 is average
@@ -112,6 +64,5 @@ f.close()
 
 # -> Project Check #2
 #Find PEG
-#DPR
 #Dividend Yield (could be #1?)
 #Compare EPS to other companies to determine if its good or not
---
+++ GoogleScraper.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup 
+from urllib.request import Request, urlopen
+import re
+
+class GoogleScraper:
+    def __init__(self, ticker):
+        self.ticker = ticker    
+        self.url = f"https://www.google.com/finance/quote/{ticker}:NYSE?hl=en"
+        self.req = Request(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+
+        self.webpage = urlopen(self.req).read()
+        self.soup = BeautifulSoup(self.webpage,'html.parser')
+
+        self.pdiv = self.soup.find_all('div', {'jsaction': 'JIbuQc:YxFE3d(IAcfIc),crCqv(kl0Ckd),sko5qc(vsmyDc);rcuQ6b:npT2md;GvneHb:GvneHb', 'jscontroller': 'glDwzc', 'class': 'GRaO7d'})
+        self.table = self.pdiv[0].contents[3]
+
+    #tContents = table.contents[3]
+    def getEPS(self):
+        for tr in self.table:
+            for td in tr:
+                if (td.find(text=re.compile("Diluted EPS"))):
+                    output = td.text
+                    ratio = td.parent.find_all('td')[1].text
+        return(output + ": " + ratio)
+
+    def getNetIncome(self):
+        NIrow = self.table.contents[2]
+        NITitle = NIrow.find('div').text
+        NetIncome = NIrow.contents[1].text
+        NetGrowth = NIrow.contents[2].find('span').text
+        print(NITitle + ": " + NetIncome)
+        print(NetGrowth)
+        NetGrowth = NetGrowth.replace('%', '')
+        NetGrowth = float(NetGrowth)
+        if (NetGrowth >= 20):
+            return('Net Income Growth is good!')
+        elif(NetGrowth >= 10):
+            return('Net Income Growth is healthy, but could be better.')
+        elif(NetGrowth >= 5):
+            return('Net Income Growth is low.')
+        elif(NetGrowth >= 0):
+            return('Net Income Growth is not good.')
+        else:
+            return('Net Income Growth is negative. Not good at all!')
+
+    def getPERatio(self):
+        stockInfo = self.soup.find('div', {'class': "eYanAe", 'role': 'region', 'aria-labelledby': 'key-stats-heading'})
+        stockInfo.find_all('div')
+        infoRows = stockInfo.find_all('div', class_='gyFHrc')
+        PErow = infoRows[5]
+        PETitle = PErow.contents[0].contents[0].text
+        PERatio = PErow.contents[1].text
+        print(PETitle + ": " + PERatio)
+        if (int(float(PERatio)) >= 20):
+            return('P/E ratio is good!')
+        else:
+            return('P/E ratio is not good!')
+
+
+    def getDivYield(self):
+        stockInfo = self.soup.find('div', {'class': "eYanAe", 'role': 'region', 'aria-labelledby': 'key-stats-heading'})
+        stockInfo.find_all('div')
+        infoRows = stockInfo.find_all('div', class_='gyFHrc')
+        DivRow = infoRows[6]
+        DivTitle = DivRow.contents[0].contents[0].text
+        DivYield = DivRow.contents[1].text
+        return(DivTitle + ": " + DivYield)
